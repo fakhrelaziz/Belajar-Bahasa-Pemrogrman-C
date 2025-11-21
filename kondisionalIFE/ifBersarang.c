9bf26298c1f4dad39c3eb6b2dc9faecedd3c3e14
Fakhrel Aziz Maarif <fakhrelazizmaarif@gmail.com>
2025-11-21 13:35:30 +0700

    Fakhrel: Add new file kondisionalIFE

---
+++ kondisionalIFE/ifBersarang.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int main(){
+     printf("\n====CEK TAHUN KABISAT====\n");
+     int tahun;
+     printf("Inputkan Tahun: ");
+     scanf("%d", &tahun);
+
+     /* KALAU MENGGGUNAKAN LOGIKA OPERATOR
+     if ((tahun % 4 == 0 || tahun % 100 == 0) && tahun % 400 == 0){
+          printf("Maka tahun %d adalah tahun kabisat", tahun);
+     }else {
+          printf("tahun %d bukan kabisat", tahun);
+     }
+     */
+    /* if (tahun % 4 == 0){
+          if (tahun % 100 == 0)
+          {
+               if (tahun % 400 == 0)
+               {
+                    printf("Tahun %d adalah tahun Kabisat", tahun);
+               }else {printf("Tahun %d adalah BUKAN tahun Kabisat", tahun);}
+               
+          }else { printf("Tahun %d adalah tahun Kabisat", tahun);}
+               
+          
+     }else {printf("Tahun %d adalah BUKAN tahun Kabisat", tahun);}
+     */
+
+     printf("%d %s tahun kabisat\n", 
+           tahun, ((tahun % 4 == 0 && tahun % 100 != 0) || tahun % 400 == 0)
+           ? "adalah" :"BUKAN");
+           
+     return 0;
+
+}
